653cbb6efc7f60a5391837f20c63457ab71c598d
Vova Abdrakhmanov <369565@gmail.com>
2020-10-09 23:13:44 +0400

    Fibonacci

---
+++ cpp/fib.cc
@@ -0,0 +1,58 @@
+#include <iostream>
+
+struct fib;
+struct fib1;
+struct fib2;
+
+struct fib
+{
+    int n;
+    fib1 *f1;
+    fib();
+    int next();
+};
+
+struct fib1
+{
+    int n;
+    fib2 *f2;
+    fib1();
+    int next();
+};
+
+struct fib2
+{
+    fib *f;
+    fib1 *f1;
+    fib2();
+    int next();
+};
+
+fib::fib(): n(0), f1(NULL) {}
+int fib::next() {
+    if (n == 0) {n = 1; return 1;}
+    if (f1 == NULL) {f1 = new fib1();}
+    return f1->next();
+}
+
+fib1::fib1(): n(0), f2(NULL) {}
+int fib1::next() {
+    if (n == 0) {n = 1; return 1;}
+    if (f2 == NULL) {f2 = new fib2();}
+    return f2->next();
+}
+
+fib2::fib2(): f(NULL), f1(NULL) {}
+int fib2::next() {
+    if (f == NULL) {f = new fib();}
+    if (f1 == NULL) {f1 = new fib1();}
+    return f->next() + f1->next();
+}
+
+int main() {
+    fib f;
+    for (int i = 0; i < 10; ++i)
+    {
+        std::cout << f.next() << std::endl;
+    }
+}
